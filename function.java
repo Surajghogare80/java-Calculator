0356d746bd62c79c91341dff935aae971849ca8f
Surajghogare80 <surajghogare1001@gmail.com>
2025-02-05 22:20:10 +0530

    product

--- function.java
+++ function.java
@@ -13,6 +13,24 @@ public class function {
     //     System.out.println("sum :" + sum) ;
     // }
 
-   
+    public static int factorial(int n ,int r){
+        int f=1;
+        for (int i=1;i<=n;i++){
+            f = f*i;
+        }
+        return f;
+     }
+
+     public static int binCoeff(int n ,int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binCoeff = fact_n / (fact_r * fact_nmr);
+     }
+
+     public static void main (String Args[]){
+        System.out.println(binCoeff(5,2));
+     }
 
 }
--- swapping.java
+++ swapping.java
@@ -14,20 +14,68 @@ public class swapping {
     // }
 
 
-    public static int swap(int a,int b){
-        int temp = a;
-        a = b;
-        b = temp;
+    // public static int swap(int a,int b){
+    //     int temp = a;
+    //     a = b;
+    //     b = temp;
 
-        System.out.print("a =" + a);
-        System.out.print("b =" + b);
-        return b;
+    //     System.out.print("a =" + a);
+    //     System.out.print("b =" + b);
+    //     return b;
+    // }
+
+    // public static void main (String Args[]){
+    //     int a = 10;
+    //     int b = 15;
+    //     swap(a , b);
+    // }
+
+    // public static int multiplication(int a , int b){
+    //     int prod = a * b;
+       
+    //     return prod;
+    // }
+
+    // public static void main (String Args[]){
+    //     int a = 10;
+    //     int b = 10;
+    //     int prod = multiplication(a , b);
+    //     System.out.println("prod "+ prod);
+    //     prod =  multiplication(12 , 12);
+    //     System.out.println("prod "+ prod);
+ 
+    // }
+
+
+    //factorization;
+
+    public static int factorial(int n ){
+        int f = 1;
+
+        for(int i=1 ; i<=n ; i++){
+            f = f*i;
+        }
+        return f;
+    }
+
+    public static int binCoeff(int n , int r){
+        int fact_n =  factorial(n);
+        int fact_r =  factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binCoeff = fact_n / (fact_r * fact_nmr);
+
+        return binCoeff ;
+
     }
 
     public static void main (String Args[]){
-        int a = 10;
-        int b = 15;
-        swap(a , b);
+        System.out.print(binCoeff(5,2));
     }
+
+    
+
+
+
 
 }
