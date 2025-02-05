dbc48ed4014bcea724c47500cc1e4f090b088b00
Surajghogare80 <surajghogare1001@gmail.com>
2025-02-05 22:36:25 +0530

    prime no code

--- swapping.java
+++ swapping.java
@@ -49,29 +49,49 @@ public class swapping {
 
     //factorization;
 
-    public static int factorial(int n ){
-        int f = 1;
+    // public static int factorial(int n ){
+    //     int f = 1;
 
-        for(int i=1 ; i<=n ; i++){
-            f = f*i;
+    //     for(int i=1 ; i<=n ; i++){
+    //         f = f*i;
+    //     }
+    //     return f;
+    // }
+
+    // public static int binCoeff(int n , int r){
+    //     int fact_n =  factorial(n);
+    //     int fact_r =  factorial(r);
+    //     int fact_nmr = factorial(n-r);
+
+    //     int binCoeff = fact_n / (fact_r * fact_nmr);
+
+    //     return binCoeff ;
+
+    // }
+
+    // public static void main (String Args[]){
+    //     System.out.print(binCoeff(5,2));
+    // }
+
+    public static boolean isPrime(int n){
+        boolean isPrime= true;
+        for (int i=2;i<=n-1;i++){
+            if(n % i == 0){
+                isPrime =false;
+                break;
+            }
+         
+
         }
-        return f;
-    }
-
-    public static int binCoeff(int n , int r){
-        int fact_n =  factorial(n);
-        int fact_r =  factorial(r);
-        int fact_nmr = factorial(n-r);
-
-        int binCoeff = fact_n / (fact_r * fact_nmr);
-
-        return binCoeff ;
-
+        return isPrime;
     }
 
     public static void main (String Args[]){
-        System.out.print(binCoeff(5,2));
+        System.out.println(isPrime(5));
     }
+
+
+    
 
     
 
