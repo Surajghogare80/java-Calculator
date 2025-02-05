f3667906014051bac11b22c6174693c3ea879b5e
Surajghogare80 <surajghogare1001@gmail.com>
2025-02-05 16:48:19 +0530

    functions in java

---
+++ function.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class function {
+    public static int sum(int a, int b){
+        int add =  a  +  b;
+        return  add ;
+
+    }
+    public static void main(String Args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = sum(a,b);
+        System.out.println("sum :" + sum) ;
+    }
+
+}
---
+++ swapping.java
@@ -0,0 +1,16 @@
+public class swapping {
+    public static void main(String  Args[]){
+
+        int a = 10;
+        int b = 15;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.print("a ="+ a);
+        System.out.print("b ="+ b);
+
+    }
+
+}
