f8314a594adc575871e5cf17c11ad694998d1781
Surajghogare80 <surajghogare1001@gmail.com>
2025-02-05 13:17:10 +0530

    loops in java

--- loops.java
+++ loops.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class loops {
     public static void main (String args[]){
 
-       /*
+    
          int counter = 0 ;
         while (counter < 100){
             System.out.println("Hello world");
@@ -10,18 +10,18 @@ public class loops {
             counter ++ ;
 
         }
-        */
+     
 
-       /*
+ 
          int number = 0 ;
 
         while (number <= 10){
             System.out.println(number);
             number++;
         }
-        */
+    
 
-       /*
+       
          Scanner sc = new Scanner (System.in);
 
         int n = sc.nextInt();
@@ -33,132 +33,132 @@ public class loops {
             i++;
         }
         System.out.println("sum is" + sum);
-        */
+    
 
-       /*
+     
           for (int line = 1; line <= 4 ; line++){
             System.out.println("* * * *");
          }
 
-        */
+    
 
 
-        // int line =1;
+        int line =1;
 
-        // while(line<=4){
-        //     System.out.println("****");
-        //     line++;
-        // }
+        while(line<=4){
+            System.out.println("****");
+            line++;
+        }
         
-        // Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner (System.in);
 
-        // int n = sc.nextInt();
-        //  //give last digit of number
+        int n = sc.nextInt();
+         //give last digit of number
 
-        // while(n>0){
-        //     int lastdigit = n % 10 ;
-        //     System.out.print(lastdigit);
-        //     n /= 10;   //remove last digit of number 
+        while(n>0){
+            int lastdigit = n % 10 ;
+            System.out.print(lastdigit);
+            n /= 10;   //remove last digit of number 
 
-        // }
+        }
 
-    //     int n= 10899;
-    //     int rev = 0;
+        int n= 10899;
+        int rev = 0;
 
-    //    while(n>0){
-    //     int lastdigit = n % 10;
-    //     rev = (rev * 10) +lastdigit;
-    //     n/=10;
-    //    }
-    //     System.out.println(rev);
+       while(n>0){
+        int lastdigit = n % 10;
+        rev = (rev * 10) +lastdigit;
+        n/=10;
+       }
+        System.out.println(rev);
 
     
    
 
-    // for (int i=1;i<=5;i++){
-    //     if(i==3){
-    //         break;
-    //     }
-    //     System.out.println(i);
-    // }
-    // System.out.println("I am exit");
+    for (int i=1;i<=5;i++){
+        if(i==3){
+            break;
+        }
+        System.out.println(i);
+    }
+    System.out.println("I am exit");
 
 
-    //  Scanner sc = new Scanner (System.in);
+     Scanner sc = new Scanner (System.in);
 
-    //  do{
-    //     System.out.println("Enter your number");
-    //     int n = sc.nextInt();
-    //     if (n % 10 == 0 ){
-    //         continue;
-    //     }
+     do{
+        System.out.println("Enter your number");
+        int n = sc.nextInt();
+        if (n % 10 == 0 ){
+            continue;
+        }
     
-    //     System.out.println(n);
-    //  }while(true);
+        System.out.println(n);
+     }while(true);
         
 
 
-    // Scanner sc = new Scanner (System.in);
-    // int n= sc.nextInt();
+    Scanner sc = new Scanner (System.in);
+    int n= sc.nextInt();
 
-    // if(n==2){
-    //     System.out.println("It is prime number");
-    // }
-    // else{
-    //     boolean isPrime= true;
+    if(n==2){
+        System.out.println("It is prime number");
+    }
+    else{
+        boolean isPrime= true;
 
-    // for(int i=2; i<=n-1 ; i++){
-    //    if ( n % i==0){
-    //     isPrime = false;
-    //    }
-    // }
+    for(int i=2; i<=n-1 ; i++){
+       if ( n % i==0){
+        isPrime = false;
+       }
+    }
 
-    // if(isPrime == true){
-    //     System.out.println(n + "is  a prime number");
-    // }
-    // else{
-    //     System.out.println(n + "is not a prime number");
-    // }
+    if(isPrime == true){
+        System.out.println(n + "is  a prime number");
+    }
+    else{
+        System.out.println(n + "is not a prime number");
+    }
 
-    // }
+    }
 
-    // Scanner sc = new Scanner (System.in);
+    Scanner sc = new Scanner (System.in);
 
-    // int num;
-    // int evenSum=0;
-    // int oddSum=0;
+    int num;
+    int evenSum=0;
+    int oddSum=0;
 
-    // do(
+    do(
         
-    //      num = sc.nextInt();
+         num = sc.nextInt();
 
-    //     if(num % 2 ==0){
-    //        evenSum += num;
-    //        System.out.println(evenSum);
-    //     }
-    //     else{
-    //         oddSum += num;
-    //         System.out.println(oddSum);
-    //     }
-    // );
-    // while(true);
+        if(num % 2 ==0){
+           evenSum += num;
+           System.out.println(evenSum);
+        }
+        else{
+            oddSum += num;
+            System.out.println(oddSum);
+        }
+    );
+    while(true);
 
 
 
-    // for(int i=0;i<=5;i++ )   {
-    //   System.out.println("i = "+i);
+    for(int i=0;i<=5;i++ )   {
+      System.out.println("i = "+i);
 
-    // }
+    }
 
-    // Scanner sc = new Scanner (System.in);
+    Scanner sc = new Scanner (System.in);
 
-    // System.out.println("Enter the number");
-    // int n = sc.nextInt();
+    System.out.println("Enter the number");
+    int n = sc.nextInt();
 
-    // for (int i=1 ; i<=10 ; i++){
-    //     int value = n*i;
-    //     System.out.println(value);
-    // }
+    for (int i=1 ; i<=10 ; i++){
+        int value = n*i;
+        System.out.println(value);
+    }
 
 
     
